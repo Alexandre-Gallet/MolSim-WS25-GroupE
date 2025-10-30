0ce43540097e96ed931ea19abae68760aa332e07
Altay <altay_ozkan@hotmail.com.tr>
2025-10-30 20:48:11 +0300

    Encapsulation

--- src/ParticleContainer.cpp
+++ src/ParticleContainer.cpp
@@ -8,7 +8,7 @@ std::size_t ParticleContainer::size() const noexcept { return particles_.size();
 
 bool ParticleContainer::empty() const noexcept { return particles_.empty(); }
 
-void ParticleContainer::reserve(std::size_t capacity) { particles_.reserve(capacity); }
+void ParticleContainer::reserve(std::size_t capacity) { particles_.reserve(capacity); } // Can also extend if not enough capacity
 
 void ParticleContainer::clear() noexcept { particles_.clear(); }
 
@@ -17,7 +17,7 @@ Particle &ParticleContainer::addParticle(const Particle &particle) {
   return particles_.back();
 }
 
-Particle &ParticleContainer::addParticle(Particle &&particle) {
+Particle &ParticleContainer::addParticle(Particle &&particle) { // Moving variables instead of copying (move constructor)
   particles_.push_back(std::move(particle));
   return particles_.back();
 }
@@ -30,6 +30,6 @@ ParticleContainer::const_iterator ParticleContainer::begin() const noexcept { re
 
 ParticleContainer::const_iterator ParticleContainer::end() const noexcept { return particles_.end(); }
 
-ParticleContainer::const_iterator ParticleContainer::cbegin() const noexcept { return particles_.cbegin(); }
+ParticleContainer::const_iterator ParticleContainer::cbegin() const noexcept { return particles_.cbegin(); } // Avoid Writing
 
 ParticleContainer::const_iterator ParticleContainer::cend() const noexcept { return particles_.cend(); }
