83f4ada113ecabf7eedb69de7897f10cb44a13b6
Altay <altay_ozkan@hotmail.com.tr>
2025-11-07 18:28:15 +0300

    Added testing

---
+++ src/ParticleGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by altay on 7.11.2025.
+//
+
+#include "ParticleGenerator.h"
---
+++ src/ParticleGenerator.h
@@ -0,0 +1,10 @@
+//
+// Created by altay on 7.11.2025.
+//
+
+#ifndef PSEMOLDYN_PARTICLEGENERATOR_H
+#define PSEMOLDYN_PARTICLEGENERATOR_H
+
+class ParticleGenerator {};
+
+#endif  // PSEMOLDYN_PARTICLEGENERATOR_H
