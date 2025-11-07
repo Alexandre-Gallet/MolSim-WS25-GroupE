2361c6ec9269756e7076871c7fb7b81eaeee5793
Altay <altay_ozkan@hotmail.com.tr>
2025-11-08 01:46:33 +0300

    Added ParticleGenerator

--- src/ParticleGenerator.h
+++ src/ParticleGenerator.h
@@ -1,10 +1,29 @@
-//
-// Created by altay on 7.11.2025.
-//
+#pragma once
+#include "ParticleContainer.h"
+#include "Particle.h"
+#include <array>
 
-#ifndef PSEMOLDYN_PARTICLEGENERATOR_H
-#define PSEMOLDYN_PARTICLEGENERATOR_H
-
-class ParticleGenerator {};
-
-#endif  // PSEMOLDYN_PARTICLEGENERATOR_H
+class ParticleGenerator {
+public:
+  /**
+   * @brief Creates a cuboid of particles and inserts them into the given container.
+   *
+   * @param container  Container to fill with generated particles.
+   * @param origin     Lower-left-front corner of the cuboid.
+   * @param numPerDim  Number of particles per dimension (N1, N2, N3).
+   * @param spacing    Distance between adjacent particles (h).
+   * @param mass       Mass of each particle.
+   * @param velocity   Initial velocity of each particle.
+   * @param type       Type/id of the particle.
+   */
+  static void generateCuboid(
+        ParticleContainer& container,
+        const std::array<double, 3>& origin,
+        const std::array<size_t, 3>& numPerDim,
+        double h,
+        double mass,
+        const std::array<double, 3>& baseVelocity,
+        double brownianMean = 0.1,
+        int type = 0
+    );
+};
