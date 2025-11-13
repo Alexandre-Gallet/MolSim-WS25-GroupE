3be5d6280b154501f6dbb30ef8a9df973bbf2956
Alexandre Gallet <alexandregallet.2018@gmail.com>
2025-11-13 15:38:14 +0100

    Fixed linking issues, undefined behaviour in Arguments structgit status!

--- src/Simulation/MoleculeSimulation.h
+++ src/Simulation/MoleculeSimulation.h
@@ -8,9 +8,10 @@
 class MoleculeSimulation : public Simulation {
   Arguments &args;
   ParticleContainer &particles;
+
  public:
   explicit MoleculeSimulation(Arguments &args, ParticleContainer &particles);
-  ~MoleculeSimulation() override;
+  ~MoleculeSimulation() override = default;
   void runSimulation() override;
   static void plotParticles(ParticleContainer &particles, int iteration, OutputFormat format);
 };
