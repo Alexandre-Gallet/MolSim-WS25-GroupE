3d9d2b5bd69d6a2e44d1bc5e33ba7ffeb1393f79
Alexandre Gallet <alexandregallet.2018@gmail.com>
2025-12-02 00:00:49 +0100

    Moved class declaration to header file

---
+++ tests/Unit/ParticleContainerTest.h
@@ -0,0 +1,18 @@
+/*
+Test fixture providing a ParticleContainer with two particles.
+
+   This fixture is used for tests that need a pre-filled container.
+   It creates two particles at positions (0,0,0) and (1,0,0).
+ */
+class ParticleContainerFixture : public ::testing::Test {
+protected:
+    void SetUp() override {
+        std::array<double, 3> x1{0, 0, 0}, x2{1, 0, 0}, v{0, 0, 0};
+        pc.addParticle(Particle{x1, v, 1.0, 0});
+        pc.addParticle(Particle{x2, v, 1.0, 0});
+    }
+
+    //The container under test.
+
+    ParticleContainer pc;
+};
