a4282038a35ee5533b52b1efdf74679d6bc9294f
Alexandre Gallet <alexandregallet.2018@gmail.com>
2026-01-23 05:11:48 +0100

    Fixed fmt and relative path bugs with build system

--- cmake/modules/spdlog.cmake
+++ cmake/modules/spdlog.cmake
@@ -1,5 +1,8 @@
 include(FetchContent)
 
+# spdlog configuration options
+set(SPDLOG_FMT_EXTERNAL OFF CACHE BOOL "" FORCE)
+
 FetchContent_Declare(
         spdlog
         URL https://github.com/gabime/spdlog/archive/refs/tags/v1.14.1.zip
--- cmake/modules/yaml-cpp.cmake
+++ cmake/modules/yaml-cpp.cmake
@@ -3,7 +3,8 @@ include(FetchContent)
 FetchContent_Declare(
         yaml-cpp
         GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
-        GIT_TAG        0.8.0            # Pinned to stable release to avoid unexpecting breaking because of new commits to master branch
+        GIT_TAG        master           # yaml-cpp 0.8.0 is so old that gcc > 13 prohibits some behaviour.
+                                        # This is not ideal but the only way to not break configuration.
 )
 
 FetchContent_MakeAvailable(yaml-cpp)
