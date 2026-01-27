086216798f4e9255f2974f55735c10ff123f9a08
Alexandre Gallet <alexandregallet.2018@gmail.com>
2026-01-27 06:04:22 +0100

    Added executable targets for profiling and benchmarking

--- cmake/modules/vtk.cmake
+++ cmake/modules/vtk.cmake
@@ -1,27 +1,27 @@
-if(ENABLE_VTK_OUTPUT)
-    message(STATUS "VTK output enabled")
-    # VTK Library, only add required components for less unnecessary includes
-    find_package(VTK REQUIRED COMPONENTS
-            CommonCore
-            CommonDataModel
-            IOXML
-    )
-
-    if(VTK_FOUND)
-        message (STATUS "Found VTK Version: ${VTK_VERSION}")
-    else ()
-        message(FATAL_ERROR "VTK not found")
-    endif ()
-
-    if(VTK_VERSION VERSION_GREATER_EQUAL ${VTK_MIN_VERSION})
-         include_directories(${VTK_INCLUDE_DIRS})
-    else()
-        include(${VTK_USE_FILE})
-    endif ()
-
-    target_link_libraries(molsim_core
-            PUBLIC
-            ${VTK_LIBRARIES}
-    )
-    target_compile_definitions(molsim_core PUBLIC ENABLE_VTK_OUTPUT)
+# Use an interface so that we can link vtk individually against targets.
+# Remove the legacy code that is no longer needed in vtk version > 7
+if(NOT ENABLE_VTK_OUTPUT)
+    return()
 endif()
+
+message(STATUS "VTK output enabled")
+
+find_package(VTK REQUIRED COMPONENTS
+        CommonCore
+        CommonDataModel
+        IOXML
+)
+
+if(VTK_VERSION VERSION_LESS ${VTK_MIN_VERSION})
+    message(FATAL_ERROR
+            "VTK ${VTK_VERSION} found, but MolSim requires at least ${VTK_MIN_VERSION}"
+    )
+endif()
+
+message(STATUS "Found VTK Version: ${VTK_VERSION}")
+
+add_library(molsim_vtk INTERFACE)
+
+target_link_libraries(molsim_vtk INTERFACE ${VTK_LIBRARIES})
+target_include_directories(molsim_vtk INTERFACE ${VTK_INCLUDE_DIRS})
+target_compile_definitions(molsim_vtk INTERFACE ENABLE_VTK_OUTPUT)
